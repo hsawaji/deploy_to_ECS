8134b0424f16b34b2e068542f40501fd276d28d0
sawaji hiroyuki <hiroyuki.sawaji@w-it.co.jp>
2018-10-30 17:15:13 +0900

    テキスト変更

--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'deploy ECS rolling test'
+    return 'deploy ECS rolling test aaaa'
 
 if __name__ == '__main__':
     app.debug = True
